737f65f2d6feea7737249e3e83eb7526d4af3a2c
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-01-24 00:02:48 +0900

    Ch 4.4 부트스트랩 UI 적용 및 Todo 기본 기능(CRUD) 스프링 연동

---
+++ springex/src/test/java/org/zerock/springex/service/TodoServiceTests.java
@@ -0,0 +1,36 @@
+package org.zerock.springex.service;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.springex.domain.TodoVO;
+import org.zerock.springex.dto.TodoDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations="classpath:root-context.xml")
+public class TodoServiceTests {
+
+    @Autowired
+    private TodoService todoService;
+
+    @Test
+    public void testRegister() {
+
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title("Test ...")
+                .dueDate(LocalDate.now())
+                .writer("user1")
+                .build();
+
+        todoService.register(todoDTO);
+    }
+
+}
