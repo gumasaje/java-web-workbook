34195e7986290e0cee3865cea5582097bd38a3a6
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-01-27 17:59:33 +0900

    Ch 6.3 Axios를 활용한 댓글 비동기(AJAX) 통신 및 자바스크립트 이벤트 처리 구현

---
+++ b01/src/main/resources/static/js/reply.js
@@ -0,0 +1,42 @@
+async function get1(bno) {
+
+    const result = await axios.get(`/api/replies/list/${bno}`)
+
+    return result;
+}
+
+async function getList({bno, page, size, goLast}) {
+
+    const result = await axios.get(`/api/replies/list/${bno}`, {params: {page, size}})
+
+    if (goLast) {
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total / size))
+
+        return getList({bno: bno, page: lastPage, size: size})
+
+    }
+
+    return result.data
+}
+
+async function addReply(replyObj) {
+    const response = await axios.post(`/api/replies/`, replyObj)
+    return response.data
+}
+
+async function getReply(rno) {
+    const response = await axios.get(`/api/replies/${rno}`)
+    return response.data
+}
+
+async function modifyReply(replyObj) {
+
+    const response = await axios.put(`/api/replies/${replyObj.rno}`, replyObj)
+    return response.data
+}
+
+async function removeReply(rno) {
+    const response = await axios.delete(`/api/replies/${rno}`)
+    return response.data
+}
